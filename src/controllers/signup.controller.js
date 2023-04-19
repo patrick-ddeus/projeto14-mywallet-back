4a2f8c179d117f1289bb1cf0a1726d01d3a59fe9
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-04-19 00:37:55 -0300

    improve clean code

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -2,16 +2,22 @@ import UserService from "../services/user.service.js";
 import bcrypt from "bcrypt";
 
 const createUser = async (req, res) => {
-    const newUserInfo = req.user;
+    const { name, email, password } = req.body;
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(newUserInfo.password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     try {
-        await UserService.insertOneService({ ...newUserInfo, password: hashedPassword, confirmPassword: hashedPassword });
+        await UserService.insertOneService({
+            name,
+            email,
+            password: hashedPassword,
+            confirmPassword: hashedPassword
+        });
+
         res.status(201).json({ message: "Usuário criado com sucesso!" });
     } catch (err) {
         return res.status(500).json({ error: err.message });
     }
-
 };
 
 export default {
